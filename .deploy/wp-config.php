e17cc7406019d57e9bbfdf1f4891abceeddf1a39
Carlos Bensant <carlosbensant@gmail.com>
2018-06-08 03:41:23 -0400

    Sets WP_DEBUG true for Deployments.

--- .deploy/wp-config.php
+++ .deploy/wp-config.php
@@ -3,6 +3,7 @@ define('DB_NAME', 'wp-demo');
 define('DB_USER', 'mysql');
 define('DB_PASSWORD', 'a0c4f5c1bc9fe520');
 define('DB_HOST', 'dokku-mysql-wp-demo');
+define('WP_DEBUG', true);
 
 if (!defined('WP_SITEURL')) {
     define('WP_SITEURL', 'http://wp-demo.staging.spot.com.do/cms');

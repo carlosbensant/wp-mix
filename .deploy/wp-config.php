f965c3fa0812befd6b988414ab77fae17edf02b3
Carlos Bensant <carlosbensant@gmail.com>
2018-06-09 12:41:08 -0400

    Changes domain string.

--- .deploy/wp-config.php
+++ .deploy/wp-config.php
@@ -6,13 +6,13 @@ define('DB_HOST', 'dokku-mysql-wp-demo');
 define('WP_DEBUG', true);
 
 if (!defined('WP_SITEURL')) {
-    define('WP_SITEURL', 'http://wp-demo.staging.spot.com.do/cms');
+    define('WP_SITEURL', 'http://wp-demo.staging.applab.do/cms');
 }
 
 if (!defined('WP_HOME')) {
-    define('WP_HOME',    'http://wp-demo.staging.spot.com.do');
+    define('WP_HOME',    'http://wp-demo.staging.applab.do');
 }
 
 if (!defined('WP_CONTENT_URL')) {
-    define('WP_CONTENT_URL', 'http://wp-demo.staging.spot.com.do/wp-content');
+    define('WP_CONTENT_URL', 'http://wp-demo.staging.applab.do/wp-content');
 }
